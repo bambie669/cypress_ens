1c5fb2240b599a2e6c8468dfc4cf6bdfd5c29156
bambie669 <raduroman94@gmail.com>
2021-10-07 13:38:35 +0300

    video section

--- cypress/integration/test6.js
+++ cypress/integration/test6.js
@@ -9,7 +9,16 @@ describe('My first Twitch test', () => {
         cy.get('.giSGZi > .ScCoreButton-sc-1qn4ixc-0 > .ScCoreButtonLabel-sc-lh1yxp-0 > .Layout-sc-nxg1ff-0').click()
         cy.visit('https://www.twitch.tv/richelletest')
         cy.get('[data-index="3"] > .ScInteractive-sc-18v7095-0 > .fiEXZq > .ScTextWrapper-sc-18v7095-1 > .CoreText-sc-cpl358-0').click()
-        cy.get('.ScFace-sc-cdc1ai-4').click()
-        cy.get(':nth-child(5) > .eVJikG > .lfUmKp > .ScCoreLink-sc-udwpw5-0 > .Layout-sc-nxg1ff-0 > .CoreText-sc-cpl358-0').click({force: true})
+        // cy.get('.ScFace-sc-cdc1ai-4').click()
+        // cy.get(':nth-child(5) > .eVJikG > .lfUmKp > .ScCoreLink-sc-udwpw5-0 > .Layout-sc-nxg1ff-0 > .CoreText-sc-cpl358-0').click()
+        cy.get('.cxWlKh > .eVJikG > .lfUmKp > .ScCoreLink-sc-udwpw5-0 > .Layout-sc-nxg1ff-0 > .CoreText-sc-cpl358-0').click()
+        cy.go('back')
+        cy.get(':nth-child(3) > .eVJikG > .lfUmKp > .ScCoreLink-sc-udwpw5-0 > .Layout-sc-nxg1ff-0 > .CoreText-sc-cpl358-0').click()
+        cy.go('back')
+        cy.get(':nth-child(4) > .eVJikG > .lfUmKp > .ScCoreLink-sc-udwpw5-0 > .Layout-sc-nxg1ff-0 > .CoreText-sc-cpl358-0').click()
+        cy.go('back')
+        cy.get(':nth-child(5) > .eVJikG > .lfUmKp > .ScCoreLink-sc-udwpw5-0 > .Layout-sc-nxg1ff-0 > .CoreText-sc-cpl358-0').click()
+
     })
+
 })
